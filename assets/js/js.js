e4ffe7ec312c6623d180b404e5fc1daa3a646b11
quangnien <quangnien1101@gmail.com>
2022-11-10 17:21:20 +0700

    tt

--- assets/js/js.js
+++ assets/js/js.js
@@ -41,5 +41,5 @@ modalContainer.addEventListener('click', function(event){
 })
 
 function openNewBrowser() {
-    window.open("https://www.w3schools.com", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=1200,height=600");
+    window.open("https://www.google.com.hk/", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=1200,height=600");
 }
